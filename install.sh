a6a4c33b3c0ba8c6467a2a9ac43c51c307948f23
renomanzzz <renomansss28@gmail.com>
2025-12-05 02:51:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 RED="\e[31m"
 GREEN="\e[32m"
@@ -10,50 +10,21 @@ clear
 
 echo -e "${CYAN}
 ╔════════════════════════════════════════╗
-║           GEMPROXY    ANDROID          ║
-║               by RENOMANSSS            ║
+║      GEMPROXY ANDROID - TERMUX         ║
+║            by RENOMANSSS               ║
 ╚════════════════════════════════════════╝
 ${END}"
 
 sleep 1
 
-# Root check
-if [[ "$EUID" -ne 0 ]]; then
-   echo -e "${YELLOW}[!] No root permissions — running with sudo...${END}"
-   sudo bash "$0"
-   exit
-fi
+echo -e "${CYAN}[*] Updating packages...${END}"
+pkg update -y && pkg upgrade -y
 
-# detekt os
-PKG_MANAGER=""
-
-if command -v apt > /dev/null; then
-    PKG_MANAGER="apt"
-elif command -v pacman > /dev/null; then
-    PKG_MANAGER="pacman"
-elif command -v pkg > /dev/null; then
-    PKG_MANAGER="pkg" # Termux
-else
-    echo -e "${RED}❌ Unsupported package manager${END}"
-    exit 1
-fi
-
-echo -e "${GREEN}[+] Package manager: $PKG_MANAGER${END}"
-
-# installllll
-echo -e "${CYAN}[*] Installing required packages...${END}"
-
-if [[ "$PKG_MANAGER" == "apt" ]]; then
-    apt update -y
-    apt install -y wget curl openssl libssl-dev unzip
-elif [[ "$PKG_MANAGER" == "pacman" ]]; then
-    pacman -Sy --noconfirm wget curl openssl
-elif [[ "$PKG_MANAGER" == "pkg" ]]; then
-    pkg update -y
-    pkg install -y wget curl openssl
-fi
+echo -e "${CYAN}[*] Installing dependencies...${END}"
+pkg install -y wget curl openssl unzip
 
 sleep 1
+
 
 if [ -f "proxy" ]; then
     echo -e "${YELLOW}[!] Old proxy found — deleting...${END}"
@@ -61,13 +32,15 @@ if [ -f "proxy" ]; then
     sleep 1
 fi
 
+
 echo -e "${CYAN}[*] Downloading proxy...${END}"
 wget --show-progress -q --content-disposition https://github.com/renomanzzz/Gemandroid/raw/refs/heads/main/proxy
+
 
 chmod +x proxy
 
 echo ""
-echo -e "${GREEN}  GemProxy installed successfully!${END}"
-echo -e "${GREEN}Run it with: ${CYAN}./proxy${END}"
-echo -e "${YELLOW}Buy license from: ${CYAN} https://discord.gg/hTu59sdDg6${END}"
+echo -e "${GREEN}✅ GemProxy installed successfully on Termux!${END}"
+echo -e "${GREEN}Run it using: ${CYAN}./proxy${END}"
+echo -e "${YELLOW}Buy license from: ${CYAN}https://discord.gg/hTu59sdDg6${END}"
 echo ""
